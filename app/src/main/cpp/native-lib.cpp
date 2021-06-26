f92e51fa0446ed1fc49f0aa4d2e17ccfe0952f07
chenpeng <chen1373228@gmail.com>
2021-06-26 21:00:48 +0800

    添加dlopen.c文件

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,16 @@
 #include <jni.h>
 #include <string>
+#include "dlopen.h"
 
-extern "C" JNIEXPORT jstring JNICALL
+extern "C" jstring
 Java_com_king_cp_monitor_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
+    //获取thread的锁对象，通过锁对象获取当前持有的thread
+    //mirror::Object* monitor::GetContendMonitor(Thread* thread)
+    void *so_addr = ndk_dlopen("libart.so");
+
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
